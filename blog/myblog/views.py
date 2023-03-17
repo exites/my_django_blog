c401b2759e23cf14480cc1b7bf346d36d7733f5b
exites <or.strangelove@gmail.com>
2023-03-17 16:15:40 +0300

    сайт запустился без картинок шаг 5.5.2

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views import View
 
+
 class MainView(View):
     def get(self, request, *args, **kwargs):
         return render(
             request,
-            'myblog/index.html'
+            'myblog/home.html'
         )
