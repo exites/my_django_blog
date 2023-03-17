df5c12a3b9946551c5839e13f04c262acd82306c
exites <or.strangelove@gmail.com>
2023-03-17 21:33:50 +0300

    pagination

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views import View
+from django.core.paginator import Paginator
+from .models import Post
 
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        return render(
-            request,
-            'myblog/home.html'
-        )
+        posts = Post.objects.all()
+        paginator = Paginator(posts, 6)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, 'myblog/home.html', context={
+            'page_obj': page_obj
+        })
