5f37589abd3f4e7e6f68971e6a80f1a684677373
exites <or.strangelove@gmail.com>
2023-03-17 15:43:46 +0300

    kak zap

---
+++ blog/myblog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views import View
+
+class MainView(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            'myblog/index.html'
+        )
